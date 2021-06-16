b20dbae520db586f960be61aeb44688432300582
Mamadou-Faye <mamadou.faye18@uvs.edu.sn>
2021-06-16 15:26:10 +0000

    Ajout des objets et classes

--- variablesJS/script.js
+++ variablesJS/script.js
@@ -4,7 +4,6 @@ const hoursPerDay = 24;
 const minutesPerHour = 60;
 const secondsPerMinute = 60;
 
-
 // =====================================
 
 const dayInput = document.querySelector('#day-input');
@@ -23,3 +22,59 @@ calculateButton.addEventListener('click', () => {
   minutes.innerText = `${calcMinutes} minutes`;
   seconds.innerText = `${calcSeconds} seconds`;
 });
+
+// exemple de string d'interpolation
+
+// const myName = "Faye";
+
+// const salutation = `Bienvenue sur mon site ${myName}`;
+
+// alert(salutation);
+
+// Application serie episode
+// Create variables here
+// =========================================
+let episodeTitle = 'amour vrai';
+let episodeDuration = 50;
+let hasBeenWatched = true;
+
+
+// =========================================
+
+document.querySelector('#episode-info').innerText = `Episode: ${episodeTitle}
+Duration: ${episodeDuration} min
+${hasBeenWatched ? 'Already watched' : 'Not yet watched'}`
+
+
+// Section class
+// Create a class here
+// =====================================
+class episode {
+  
+  constructor(title, duration, hasBeenWatched) {
+    this.title = title;
+    this.duration = duration;
+    this.hasBeenWatched = hasBeenWatched;
+  }
+}
+
+let firstEpisode = new episode('la force de l\'amour', 45, true);
+
+let secondEpisode = new episode('l\'esclavage', 50, false);
+
+let thirdEpisode = new episode('le secret de amour', 35, true);
+
+
+// =====================================
+
+document.querySelector('#first-episode-info').innerText = `Episode: ${firstEpisode.title}
+Duration: ${firstEpisode.duration} min
+${firstEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+
+document.querySelector('#second-episode-info').innerText = `Episode: ${secondEpisode.title}
+Duration: ${secondEpisode.duration} min
+${secondEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+
+document.querySelector('#third-episode-info').innerText = `Episode: ${thirdEpisode.title}
+Duration: ${thirdEpisode.duration} min
+${thirdEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
