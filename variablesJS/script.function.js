799cc27a35f4b573f120fc48f216de6026023619
Mamadou-Faye <mamadou.faye18@uvs.edu.sn>
2021-06-30 10:04:08 +0000

    Les fonctions predefinies exemple 2

--- variablesJS/script.function.js
+++ variablesJS/script.function.js
@@ -183,3 +183,33 @@ const printStringStats = (stringToTest) => {
 
   console.log(stringStats);
 }
+
+// Voyons maintenant cette refactorisation
+
+const getWordCount = (stringToTest) => {
+  const wordArray = stringToTest.split(" ");
+  return wordArray.length;
+}
+
+const getLetterCount = (stringToTest) => {
+  const wordArray = stringToTest.split(" ");
+  let totalLetters = 0;
+  for (let word of wordArray) {
+    word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    totalLetters += word.length;
+  }
+
+  return totalLetters;
+}
+
+const getAverageWordLength = (stringToTest) => {
+  return parseFloat((getLetterCount(stringToTest) / wordCount(stringToTest)).toFixed(2));
+}
+
+const printStringStats = (stringToTest) => {
+  console.log({
+    wordCount : getWordCount(stringToTest);
+    letterCount : getLetterCount(stringToTest);
+    averageWordLength : getAverageWordLength(stringToTest);
+  });
+}
