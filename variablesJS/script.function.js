73c3460a78edf5cf2b61a7514f20f4c2fa97e923
Mamadou-Faye <mamadou.faye18@uvs.edu.sn>
2021-06-30 09:37:34 +0000

    les fonctions predefines en javaScript exemple 1

--- variablesJS/script.function.js
+++ variablesJS/script.function.js
@@ -164,3 +164,22 @@ for(let episode of episodes) {
   let newParagraph = document.createElement('p');
   newParagraph.innerText = `${episode.firstEpisode}`;
 }
+
+const printStringStats = (stringToTest) => {
+  const wordArray = stringToTest.split(" ");
+  const wordCount = wordArray.length;
+  let letterCount = 0;
+  for (let word of wordArray) {
+    word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    letterCount += word.length;
+  }
+
+  const averageWordLength = parseInt((letterCount / wordCount).toFixed(2));
+  const stringStats = {
+    wordCount : wordCount;
+    letterCount : letterCount;
+    averageWordLength : averageWordLength
+  };
+
+  console.log(stringStats);
+}
