a5255012c366ec787796aed04939945bef161217
Mamadou-Faye <mamadou.faye18@uvs.edu.sn>
2021-07-06 17:27:24 +0000

    Modification de l'animation de mon grand titre

--- variablesJS/js/script.js
+++ variablesJS/js/script.js
@@ -57,7 +57,7 @@ let monBouton = document.getElementById('btn');
 window.addEventListener('scroll', function () {
 	let value = window.scrollY;
 	// monBouton.style.marginTop = value * 1.6 + 'px';
-	monTitre.style.letterSpacing = value * 0.1 + 'px';
+	monTitre.style.letterSpacing = value * 0.05 + 'px';
 });
 
 // let description = {
