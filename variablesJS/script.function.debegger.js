0d17704e15edead5b646e1fc62e1a370e1209972
Mamadou-Faye <mamadou.faye18@uvs.edu.sn>
2021-07-07 02:02:32 +0000

    L'utilation de la console pour afficher les messages d'erreurs

--- variablesJS/script.function.debegger.js
+++ variablesJS/script.function.debegger.js
@@ -3,3 +3,10 @@ const getWordCount = (stringToTest) => {
 	return wordArray.length;
 }
 
+// Utilisons l'affichage de console pour voir ce quik passe
+const wordArray = (stringToTest) => {
+	const wordArray = stringToTest.split('');
+	console.log('Word array in getWordCount: ');
+	console.log(wordArray);
+	return wordArray.length;
+}
