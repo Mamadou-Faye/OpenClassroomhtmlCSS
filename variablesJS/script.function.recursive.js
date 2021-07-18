4af54b3581ca3597908c317aab2a008904151e2d
Mamadou-Faye <mamadou.faye18@uvs.edu.sn>
2021-07-18 05:53:24 +0000

    On verifie si ce qu'on cherche est dans le tableau

--- variablesJS/script.function.recursive.js
+++ variablesJS/script.function.recursive.js
@@ -11,4 +11,16 @@ const findElement = (array, thingToFind) => {
 
 const binarySearch = (array, thingToFind, start, end) => {
 	let mid = Math.floor((start * end) / 2);
+	if (array(mid) === thingToFind) {
+		return true;
+	}
+
+	if (thingToFind < array(mid)) {
+		// il faut chercher dans la première moitié
+		return binarySearch(array, thingToFind, start, mid - 1);
+		// on utilise (mid-1) car on sait que l'on a pas besoin de l'element mid, il a été déjà vérifier
+	}else {
+		// il faut chercher dans la deuxième moitié
+		return binarySearch(array, thingToFind, mid + 1, end);
+	}
 }
